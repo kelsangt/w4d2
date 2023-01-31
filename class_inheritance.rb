b18603155033f68f1dfcf56d2eab4c17b04fc0ca
“Kelsang” <kelsangrtsering@gmail.com>
2023-01-31 15:13:19 -0500

    Complete class_inheritance project

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -47,3 +47,17 @@ david = Employee.new("David", "TA", 10_000)
 shawna = Employee.new("Shawna", "TA", 12_000)
 darren = Manager.new("Darren", "TA Manager", 78_000)
 ned = Manager.new("Ned", "Founder", 1_000_000)
+
+david.add_boss(darren)
+shawna.add_boss(darren)
+darren.add_employees(david)
+darren.add_employees(shawna)
+darren.add_boss(ned)
+ned.add_employees(darren)
+ned.add_employees(david)
+ned.add_employees(shawna)
+
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
